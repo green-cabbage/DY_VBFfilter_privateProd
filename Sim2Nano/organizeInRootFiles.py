45ba0d8da482b5b0b4438055be8caad6c99b80b7
green-cabbage <hyeonseo_yun@hotmail.com>
2025-03-11 13:14:39 -0400

    update

--- Sim2Nano/organizeInRootFiles.py
+++ Sim2Nano/organizeInRootFiles.py
@@ -22,21 +22,24 @@ def getCrabGensimFiledict(base_path: str, dates: list, chunksize=500) -> dict:
         filelist = glob.glob(dir_path)
         # Filter out strings that contain "inLHE"
         filelist = [s for s in filelist if "inLHE" not in s]
+        # remove /eos/purdue
+        filelist = [s.replace("/eos/purdue","") for s in filelist]
         # print(f"len(filelist): {len(filelist)}")
 
         # break the list into smaller list with each size of the given chunksize
         chunks = chunk_list_iter(filelist, size=chunksize)
         for ix in range(len(chunks)):
             chunk_input_rootfiles = chunks[ix]
-            unique_output_fname = f"{date}/out_{date}_{ix}.root"
+            unique_output_fname = f"input_fnames/{date}/out_{date}_{ix}.txt"
             date_out_dict[unique_output_fname] = chunk_input_rootfiles
+            # print(f"len(chunk_input_rootfiles): {len(chunk_input_rootfiles)}")
+        print(f"date_out_dict.keys(): {date_out_dict.keys()}")
         total_file_dict[date] = date_out_dict
 
     return total_file_dict
 
 
-base_path = "/eos/purdue/store/user/hyeonseo/Run2UL/UL2018/DYJetsToLL_M-105To160_VBFFilter_TuneCP5_PSweights_13TeV-amcatnloFXFX-pythia8/gensim_crab_1/"
-# dir_path = f"{base_path}/250307_134706/*/*.root"
+base_path = "/eos/purdue/store/user/hyeonseo/Run2UL/UL2018/DYJetsToLL_M-105To160_VBFFilter_TuneCP5_PSweights_13TeV-amcatnloFXFX-pythia8/gensim_crab_1"
 
 dates = [
     "250307_134706", 
@@ -44,7 +47,18 @@ dates = [
 ]
 chunksize = 500
 file_dict = getCrabGensimFiledict(base_path, dates, chunksize=chunksize)
-print(len(file_dict))
+# print(len(file_dict))
+# print(file_dict.keys())
+# print(file_dict)
+for date_file_dict in file_dict.values():
+    for outfile_name, input_root_files in date_file_dict.items():
+        print(f"outfile_name: {outfile_name}")
+        directory = os.path.dirname(outfile_name)
+        print(f"directory: {directory}")
+        os.makedirs(directory, exist_ok=True) 
+        with open(outfile_name, "w") as f:
+            f.write(", ".join(input_root_files))
+
 
 # divide the filelist into chunksizes
 
