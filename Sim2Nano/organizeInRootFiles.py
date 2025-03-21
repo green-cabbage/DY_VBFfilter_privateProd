c7a5f6e678195d7c78bbe2d19dfb85e6ef1690e4
green-cabbage <hyeonseo_yun@hotmail.com>
2025-03-21 14:09:49 -0400

    adding input file json feature instead of directores

--- Sim2Nano/organizeInRootFiles.py
+++ Sim2Nano/organizeInRootFiles.py
@@ -50,7 +50,8 @@ def getCrabGensimFiledict(base_path: str, dates: list, chunksize=500) -> dict:
         # Filter out strings that contain "inLHE"
         filelist = [s for s in filelist if "inLHE" not in s]
         # remove /eos/purdue
-        filelist = [s.replace("/eos/purdue","") for s in filelist]
+        # filelist = [s.replace("/eos/purdue","") for s in filelist]
+        filelist = [s.replace("/eos/purdue","root://eos.cms.rcac.purdue.edu/") for s in filelist]
         # print(f"len(filelist): {len(filelist)}")
         total_filelilst += filelist
     total_file_dict = {}
@@ -68,26 +69,56 @@ def getCrabGensimFiledict(base_path: str, dates: list, chunksize=500) -> dict:
 base_path = "/eos/purdue/store/user/hyeonseo/Run2UL/UL2018/DYJetsToLL_M-105To160_VBFFilter_TuneCP5_PSweights_13TeV-amcatnloFXFX-pythia8/gensim_crab_1"
 
 dates = [ # order of which the jobs succeeded with 100% completion
-    "250307_134706", 
-    "250308_023802",
-    "250309_133626",
-    "250309_152409", # March 12 2025 out_4000.txt
-    "250310_012204", 
-    "250310_142512", # March 13 2025 out_6000.txt
+    "250307_134706", # SLURM
+    "250308_023802", # SLURM
+    "250309_133626", # SLURM
+    "250309_152409", # March 12 2025 out_4000.txt # SLURM
+    "250310_012204", # SLURM
+    "250310_142512", # March 13 2025 out_6000.txt # SLURM
+    "250310_175949", # March 13 2025 out_7000.txt # condor
+    "250309_183426", # condor
+    "250311_024801", # condor
+    "250312_140157", # March 14 2025 out_10000.txt # condor
 ]
 chunksize = 10 #50 # 250 #500
 file_dict = getCrabGensimFiledict(base_path, dates, chunksize=chunksize)
 # print(len(file_dict))
 # print(file_dict.keys())
 # print(file_dict)
+
+# # for SLURM
+# for date_file_dict in file_dict.values():
+#     for outfile_name, input_root_files in date_file_dict.items():
+#         # print(f"outfile_name: {outfile_name}")
+#         directory = os.path.dirname(outfile_name)
+#         # print(f"directory: {directory}")
+#         os.makedirs(directory, exist_ok=True) 
+#         with open(outfile_name, "w") as f:
+#             f.write("\n".join(input_root_files))
+
+# for condor
+import pickle
+import re
+
+
+
+# Sample dictionary
+dict2save = {}
+
 for date_file_dict in file_dict.values():
     for outfile_name, input_root_files in date_file_dict.items():
+        # Extract the number after "out_" and before ".txt"
+        match = re.search(r'out_(\d+)\.txt$', outfile_name)
+        number = match.group(1)
         # print(f"outfile_name: {outfile_name}")
-        directory = os.path.dirname(outfile_name)
-        # print(f"directory: {directory}")
-        os.makedirs(directory, exist_ok=True) 
-        with open(outfile_name, "w") as f:
-            f.write("\n".join(input_root_files))
+        # print(f"number: {number}")
+        dict2save[number] = input_root_files
 
+# print(f"dict2save: {dict2save}")
+# Save to pickle file
+# with open("input_fnames/input_dict.pkl", "wb") as f:
+#     pickle.dump(dict2save, f)
 
-
+import json
+with open("input_fnames/input_dict.json", "w") as fout:
+    json.dump(dict2save, fout)
